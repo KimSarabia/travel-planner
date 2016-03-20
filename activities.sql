e0e28607bd33ef3e25bbf96b0732e8a02a7cde71
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-03-19 17:37:53 -0700

    created sql dump

---
+++ activities.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.11, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: activities
+-- ------------------------------------------------------
+-- Server version	5.7.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activities`
+--
+
+DROP TABLE IF EXISTS `activities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activities` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `destination` varchar(150) DEFAULT NULL,
+  `activity` varchar(200) DEFAULT NULL,
+  `detail` varchar(200) DEFAULT NULL,
+  `note` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `activities`
+--
+
+LOCK TABLES `activities` WRITE;
+/*!40000 ALTER TABLE `activities` DISABLE KEYS */;
+INSERT INTO `activities` VALUES (2,'New York City','Kayaking on the Hudson','Hudson','Make reservation'),(4,'Mexico City','Dia de los Muertos','Day-Long Event with Interpreter','Paypal Interpreter'),(5,'Sydney','Surfing','Bring SPF','Save $5000'),(8,'Helsinki','Ice Fishing','Bring snowshoes','Call Josef'),(9,'Mexico City','Tamale Making Class','Meet at market at 9 AM','Eat light the night before');
+/*!40000 ALTER TABLE `activities` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-19 17:35:39
--- config/db.js
+++ config/db.js
@@ -2,12 +2,17 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection(process.env.JAWSDB_URL || {
     host: "localhost",
     user: "root",
     password: "test",
     database: "activities"
 });
+
+
+"mysql://tzep3bvpsplngeiv:t1sm4puaww5hrjwd@izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qxayptk5yjgb7vj3"
+"mysql://user:password@host/database"
+
 
 connection.connect(function(err){
 if(err){
